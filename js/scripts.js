547a8bca557849016de3299051e0be348a0f23b5
michelshintaku <michel.shintaku@gmail.com>
2018-10-08 21:41:49 -0300

    inclusao de json questoes

--- js/scripts.js
+++ js/scripts.js
@@ -5,79 +5,319 @@ $(document).ready(function () {
         //Incluir alteração do arquivo
         switch (valueInput) {
             case 'option1':
-                urlJson = "./json/dir.administrativo.json";
+                urlJson = "./json/dir.administrativo.00.json";
                 break;
             case 'option2':
-                urlJson = "./json/dir.constitucional.00.json";
+                urlJson = "./json/dir.administrativo.01.json";
                 break;
             case 'option3':
-                urlJson = "./json/dir.constitucional.01.json";
+                urlJson = "./json/dir.administrativo.02.json";
                 break;
             case 'option4':
-                urlJson = "./json/dir.humanos.cidadania.json";
+                urlJson = "./json/dir.administrativo.03.json";
                 break;
             case 'option5':
-                urlJson = "./json/dir.processual.penal.00.json";
+                urlJson = "./json/dir.administrativo.04.json";
                 break;
             case 'option6':
-                urlJson = "./json/dir.processual.penal.01.json";
+                urlJson = "./json/dir.administrativo.05.json";
                 break;
             case 'option7':
-                urlJson = "./json/dir.processual.penal.02.json";
+                urlJson = "./json/dir.administrativo.06.json";
                 break;
             case 'option8':
-                urlJson = "./json/dir.processual.penal.03.json";
+                urlJson = "./json/dir.administrativo.07.json";
                 break;
             case 'option9':
-                urlJson = "./json/dir.penal.json";
+                urlJson = "./json/dir.constitucional.00.json";
                 break;
             case 'option10':
-                urlJson = "./json/etica.00.json";
+                urlJson = "./json/dir.constitucional.01.json";
                 break;
             case 'option11':
-                urlJson = "./json/etica.01.json";
+                urlJson = "./json/dir.constitucional.02.json";
                 break;
             case 'option12':
-                urlJson = "./json/etica.02.json";
+                urlJson = "./json/dir.constitucional.03.json";
                 break;
             case 'option13':
-                urlJson = "./json/etica.03.json";
+                urlJson = "./json/dir.constitucional.04.json";
                 break;
             case 'option14':
-                urlJson = "./json/fisica.json";
+                urlJson = "./json/dir.constitucional.05.json";
                 break;
             case 'option15':
-                urlJson = "./json/informatica.json";
+                urlJson = "./json/dir.constitucional.06.json";
                 break;
             case 'option16':
-                urlJson = "./json/legislacao.dprf.00.json";
+                urlJson = "./json/dir.constitucional.07.json";
                 break;
             case 'option17':
-                urlJson = "./json/legislacao.dprf.01.json";
+                urlJson = "./json/dir.humanos.cidadania.00.json";
                 break;
             case 'option18':
-                urlJson = "./json/legislacao.dprf.02.json";
+                urlJson = "./json/dir.humanos.cidadania.01.json";
                 break;
             case 'option19':
-                urlJson = "./json/legislacao.dprf.03.json";
+                urlJson = "./json/dir.humanos.cidadania.02.json";
                 break;
             case 'option20':
-                urlJson = "./json/legislacao.especial.json";
+                urlJson = "./json/dir.humanos.cidadania.03.json";
                 break;
             case 'option21':
-                urlJson = "./json/matematica.json";
+                urlJson = "./json/dir.humanos.cidadania.04.json";
                 break;
             case 'option22':
-                urlJson = "./json/portugues.json";
+                urlJson = "./json/dir.processual.penal.00.json";
                 break;
             case 'option23':
-                urlJson = "./json/nocoes.administracao.json";
+                urlJson = "./json/dir.processual.penal.01.json";
                 break;
             case 'option24':
-                urlJson = "./json/nocoes.estatistica.json";
+                urlJson = "./json/dir.processual.penal.02.json";
+                break;
+            case 'option25':
+                urlJson = "./json/dir.processual.penal.03.json";
+                break;
+            case 'option26':
+                urlJson = "./json/dir.processual.penal.04.json";
+                break;
+            case 'option27':
+                urlJson = "./json/dir.processual.penal.05.json";
+                break;
+            case 'option28':
+                urlJson = "./json/dir.processual.penal.06.json";
+                break;
+            case 'option29':
+                urlJson = "./json/dir.processual.penal.07.json";
+                break;
+            case 'option30':
+                urlJson = "./json/dir.processual.penal.08.json";
+                break;
+            case 'option31':
+                urlJson = "./json/dir.processual.penal.09.json";
+                break;
+            case 'option32':
+                urlJson = "./json/dir.penal.00.json";
+                break;
+            case 'option33':
+                urlJson = "./json/dir.penal.01.json";
+                break;
+            case 'option34':
+                urlJson = "./json/dir.penal.02.json";
+                break;
+            case 'option35':
+                urlJson = "./json/dir.penal.03.json";
+                break;
+            case 'option36':
+                urlJson = "./json/dir.penal.04.json";
+                break;
+            case 'option37':
+                urlJson = "./json/dir.penal.05.json";
+                break;
+            case 'option38':
+                urlJson = "./json/dir.penal.06.json";
+                break;
+            case 'option39':
+                urlJson = "./json/dir.penal.07.json";
+                break;
+            case 'option40':
+                urlJson = "./json/dir.penal.08.json";
+                break;
+            case 'option41':
+                urlJson = "./json/dir.penal.09.json";
+                break;
+            case 'option42':
+                urlJson = "./json/etica.00.json";
+                break;
+            case 'option43':
+                urlJson = "./json/etica.01.json";
+                break;
+            case 'option44':
+                urlJson = "./json/etica.02.json";
+                break;
+            case 'option45':
+                urlJson = "./json/etica.03.json";
+                break;
+            case 'option46':
+                urlJson = "./json/fisica.00.json";
+                break;
+            case 'option47':
+                urlJson = "./json/fisica.01.json";
+                break;
+            case 'option48':
+                urlJson = "./json/fisica.02.json";
+                break;
+            case 'option49':
+                urlJson = "./json/informatica.00.json";
+                break;
+            case 'option50':
+                urlJson = "./json/informatica.01.json";
+                break;
+            case 'option51':
+                urlJson = "./json/informatica.02.json";
+                break;
+            case 'option52':
+                urlJson = "./json/informatica.03.json";
+                break;
+            case 'option53':
+                urlJson = "./json/informatica.04.json";
+                break;
+            case 'option54':
+                urlJson = "./json/legislacao.dprf.00.json";
+                break;
+            case 'option55':
+                urlJson = "./json/legislacao.dprf.01.json";
+                break;
+            case 'option56':
+                urlJson = "./json/legislacao.dprf.02.json";
+                break;
+            case 'option57':
+                urlJson = "./json/legislacao.dprf.03.json";
+                break;
+            case 'option58':
+                urlJson = "./json/legislacao.dprf.04.json";
+                break;
+            case 'option59':
+                urlJson = "./json/legislacao.especial.00.json";
+                break;
+            case 'option60':
+                urlJson = "./json/legislacao.especial.01.json";
+                break;
+            case 'option61':
+                urlJson = "./json/legislacao.especial.02.json";
+                break;
+            case 'option62':
+                urlJson = "./json/legislacao.especial.03.json";
+                break;
+            case 'option63':
+                urlJson = "./json/legislacao.especial.04.json";
+                break;
+            case 'option64':
+                urlJson = "./json/legislacao.especial.05.json";
+                break;
+            case 'option65':
+                urlJson = "./json/legislacao.especial.06.json";
+                break;
+            case 'option66':
+                urlJson = "./json/legislacao.especial.07.json";
+                break;
+            case 'option67':
+                urlJson = "./json/legislacao.especial.08.json";
+                break;
+            case 'option68':
+                urlJson = "./json/legislacao.especial.09.json";
+                break;
+            case 'option69':
+                urlJson = "./json/legislacao.especial.10.json";
+                break;
+            case 'option70':
+                urlJson = "./json/legislacao.especial.11.json";
+                break;
+            case 'option71':
+                urlJson = "./json/legislacao.especial.12.json";
+                break;
+            case 'option72':
+                urlJson = "./json/matematica.00.json";
+                break;
+            case 'option73':
+                urlJson = "./json/matematica.01.json";
+                break;
+            case 'option74':
+                urlJson = "./json/matematica.02.json";
+                break;
+            case 'option75':
+                urlJson = "./json/matematica.03.json";
+                break;
+            case 'option76':
+                urlJson = "./json/matematica.04.json";
+                break;
+            case 'option77':
+                urlJson = "./json/matematica.05.json";
+                break;
+            case 'option78':
+                urlJson = "./json/portugues.00.json";
+                break;
+            case 'option79':
+                urlJson = "./json/portugues.01.json";
+                break;
+            case 'option80':
+                urlJson = "./json/portugues.02.json";
+                break;
+            case 'option81':
+                urlJson = "./json/portugues.03.json";
+                break;
+            case 'option82':
+                urlJson = "./json/portugues.04.json";
+                break;
+            case 'option83':
+                urlJson = "./json/portugues.05.json";
+                break;
+            case 'option84':
+                urlJson = "./json/nocoes.administracao.00.json";
+                break;
+            case 'option85':
+                urlJson = "./json/nocoes.administracao.01.json";
+                break;
+            case 'option86':
+                urlJson = "./json/nocoes.administracao.02.json";
+                break;
+            case 'option87':
+                urlJson = "./json/nocoes.administracao.03.json";
+                break;
+            case 'option88':
+                urlJson = "./json/nocoes.administracao.04.json";
+                break;
+            case 'option89':
+                urlJson = "./json/nocoes.administracao.05.json";
+                break;
+            case 'option90':
+                urlJson = "./json/nocoes.administracao.06.json";
+                break;
+            case 'option91':
+                urlJson = "./json/nocoes.administracao.07.json";
+                break;
+            case 'option92':
+                urlJson = "./json/nocoes.estatistica.00.json";
+                break;
+            case 'option93':
+                urlJson = "./json/nocoes.estatistica.01.json";
+                break;
+            case 'option94':
+                urlJson = "./json/nocoes.estatistica.02.json";
+                break;
+            case 'option95':
+                urlJson = "./json/nocoes.estatistica.03.json";
+                break;
+            case 'option96':
+                urlJson = "./json/nocoes.estatistica.04.json";
+                break;
+            case 'option97':
+                urlJson = "./json/nocoes.estatistica.05.json";
+                break;
+            case 'option98':
+                urlJson = "./json/simulado.00.json";
+                break;
+            case 'option99':
+                urlJson = "./json/simulado.01.json";
+                break;
+            case 'option100':
+                urlJson = "./json/simulado.02.json";
+                break;
+            case 'option101':
+                urlJson = "./json/simulado.03.json";
+                break;
+            case 'option102':
+                urlJson = "./json/simulado.04.json";
+                break;
+            case 'option103':
+                urlJson = "./json/simulado.05.json";
+                break;
+            case 'option104':
+                urlJson = "./json/simulado.06.json";
                 break;
             default:
-                urlJson = "./json/simulado.pf.json";
+                urlJson = "./json/simulado.07.json";
         }
     });
 
